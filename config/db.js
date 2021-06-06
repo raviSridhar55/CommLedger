4bb802bdfa32e004c8c2513e5c89a458fabc40d9
Tanish Vemu <www.vtani74@gmail.com>
2021-06-06 18:14:18 +0530

    fixed get contacts

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
@@ -10,9 +10,11 @@ const connectDB = async () => {
       useFindAndModify: false,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+
+    console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
+    // Exit process with failure
     process.exit(1);
   }
 };
